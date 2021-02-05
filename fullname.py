7494c8c4a241b59d66d8a2bde076171cea42c87f
Karina Munoz <munozka@oregonstate.edu>
2021-02-05 15:44:48 -0800

    added name formatting

--- fullname.py
+++ fullname.py
@@ -1,9 +1,14 @@
 # generates a full name when provided first and last name as inputs
 
 def fullname(f, l):
-    return f + " " + l
+    full_name = f + " " + l
+    return full_name.title()
 
 first = input("Enter first name: ")
+if first == '':
+    raise ValueError("empty name")
 last = input("Enter last name: ")
+if last == '':
+    raise ValueError("empty name")
 
 print("Full name: ", fullname(first, last))
