cdbb29671f8f90885d58a6a6aee1fbfa81e97413
Karina Munoz <munozka@oregonstate.edu>
2021-02-05 15:45:16 -0800

    added descriptions

--- average.py
+++ average.py
@@ -1,7 +1,10 @@
 # calculate the average of elements in a list
 
 def avg(l):
-    return sum(l) / len(l)
+    if len(l) == 0:
+        raise ValueError("list is empty")
+    else:
+        return sum(l) / len(l)
 
 lst = []
 count = 1
@@ -10,8 +13,11 @@ n = int(input("Enter number of elements in list: "))
 
 for i in range (0, n):
     element = int(input("Element %d: " %count))
-    lst.append(element)
-    count += 1
+    if type(element) not in [int, float]:
+        raise TypeError("list element must be valid int or float")
+    else:
+        lst.append(element)
+        count += 1
 
 average = avg(lst)
 
